d333a3d4240ebcd8a9212978454cedd69f3f2472
THAARUNKARTHICK <tharunkarthick001@gmail.com>
2023-07-31 18:41:38 +0530

    Added Two projects

---
+++ problem_01/frontend/numbers/src/App.js
@@ -0,0 +1,12 @@
+const App = () =>{
+  return (
+    <div>
+      <li><a href="http://localhost:4000/numbers/prime">Prime Numbers</a></li>
+      <li><a href="http://localhost:4000/numbers/fibo">Fibonacci Numbers</a></li>
+      <li><a href="http://localhost:4000/numbers/odd">Odd Numbers</a></li>
+      <li><a href="http://localhost:4000/numbers/rand">Random Numbers</a></li>
+    </div>
+  )
+}
+
+export default App; 
